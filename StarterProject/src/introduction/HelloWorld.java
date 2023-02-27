06dc7394d5fd96d70c84a07c41958268ffc87ae3
jelenapetrovacki <jelenapetrovacki@uns.ac.rs>
2023-02-27 08:43:01 +0100

    Vežbe 1 - 27.2.2023.

--- StarterProject/src/introduction/HelloWorld.java
+++ StarterProject/src/introduction/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello world - Vežbe 1");
 	}
 
 }
